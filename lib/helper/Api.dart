2f252ff2350d00b7e2d075647ba0a540c7249fa9
dell <elocklymuhamed@gmail.com>
2024-01-24 19:52:04 +0200

    update api class

--- lib/helper/Api.dart
+++ lib/helper/Api.dart
@@ -1,12 +1,19 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:store_app/models/product_model.dart';
 
 class Api {
-  Future<List> get(String uri) async {
-    http.Response response =
-        await http.get(Uri.parse(uri));
+  Future<List> get({required uri, @required String? token}) async {
+
+     Map<String, String> header = {};
+    if (token != null) {
+      header.addAll(
+        {'Authorization': 'Bearer $token'},
+      );
+    }
+    http.Response response = await http.get(Uri.parse(uri),headers: header);
 
     if (response.statusCode == 200) {
       List<dynamic> data = jsonDecode(response.body);
@@ -15,6 +22,31 @@ class Api {
     } else {
       throw Exception(
         'ther is a problem ${response.statusCode}',
+      );
+    }
+  }
+
+  Future<dynamic> post(
+      {required String url,
+      @required dynamic body,
+      @required String? token}) async {
+    Map<String, String> header = {};
+    if (token != null) {
+      header.addAll(
+        {'Authorization': 'Bearer $token'},
+      );
+    }
+    http.Response response = await http.post(
+      Uri.parse(url),
+      body: body,
+      headers: header,
+    );
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = jsonDecode(response.body);
+      return data;
+    } else {
+      throw Exception(
+        'Ther is a problem with status code ${response.statusCode} with body ${response.body}',
       );
     }
   }

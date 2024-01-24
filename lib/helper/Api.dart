a8adc755e35d5ca25019fddd7ee8f4a08902c8cc
dell <elocklymuhamed@gmail.com>
2024-01-24 21:53:20 +0200

    last update

--- lib/helper/Api.dart
+++ lib/helper/Api.dart
@@ -26,28 +26,4 @@ class Api {
     }
   }
 
-  Future<dynamic> post(
-      {required String url,
-      @required dynamic body,
-      @required String? token}) async {
-    Map<String, String> header = {};
-    if (token != null) {
-      header.addAll(
-        {'Authorization': 'Bearer $token'},
-      );
-    }
-    http.Response response = await http.post(
-      Uri.parse(url),
-      body: body,
-      headers: header,
-    );
-    if (response.statusCode == 200) {
-      Map<String, dynamic> data = jsonDecode(response.body);
-      return data;
-    } else {
-      throw Exception(
-        'Ther is a problem with status code ${response.statusCode} with body ${response.body}',
-      );
-    }
-  }
 }

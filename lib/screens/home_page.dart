2dcab06607fd407f240b3c212a805b001e789947
dell <elocklymuhamed@gmail.com>
2024-01-24 21:55:38 +0200

    add home page

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  static String id = 'HomePage';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
